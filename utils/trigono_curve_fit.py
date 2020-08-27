3e87c1a7f3c081877b3939874c59a026cdebcf3e
肖尚华 <hallazie@gmail.com>
2020-08-27 23:42:36 +0800

    init trigono fit

---
+++ utils/trigono_curve_fit.py
@@ -0,0 +1,46 @@
+# --*-- coding:utf-8 --*--
+# @author: Xiao Shanghua
+# @contact: hallazie@outlook.com
+# @file: trigono_curve_fit.py
+# @time: 2020/8/27 23:34
+# @desc:
+
+import matplotlib.pyplot as plt
+
+import numpy
+import math
+
+
+class TrigonometricCurveFit:
+    """
+    三角函数拟合曲线
+    """
+    def __init__(self):
+        self.expression = []
+
+    def fit(self, source):
+        """
+        返回拟合后的曲线
+        :return:
+        """
+        pass
+
+    def build(self, pi_list):
+        """
+        构建表达式，pi list=[1, 2, 3, ... 1/2, 1/4, ...] etc
+        :param pi_list:
+        :return:
+        """
+        pass
+
+    def optimize(self):
+        """
+        使用最小二乘法优化
+        :return:
+        """
+        pass
+
+
+
+
+
