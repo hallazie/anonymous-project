4aec6c911e2b1537503d921b552e154a0f913c73
hallazie <hallazie@outlook.com>
2020-08-28 14:04:09 +0800

    --amend

--- utils/trigono_curve_fit.py
+++ utils/trigono_curve_fit.py
@@ -14,6 +14,7 @@ class TrigonometricCurveFit:
     """
     三角函数拟合曲线
     """
+
     def __init__(self):
         self.size = None
         self.ws1 = None
@@ -50,40 +51,57 @@ class TrigonometricCurveFit:
         :return:
         """
         for it in range(iter_nums):
+            d_ws1 = [0 for i in range(self.size)]
+            d_ws2 = [0 for i in range(self.size)]
+            d_bs1 = [0 for i in range(self.size)]
+            d_wc1 = [0 for i in range(self.size)]
+            d_wc2 = [0 for i in range(self.size)]
+            d_bc1 = [0 for i in range(self.size)]
+            d = 0
+            loss = 0
             for x, y in zip(x_list, y_list):
                 p = self._tri(x)
-                print('current loss: %s' % ((p - y) ** 2))
-                d = 2 * (p - y)
-                d_ws1 = [np.cos(self.ws1[i] * x + self.bs1[i]) for i in range(self.size)]
-                d_ws2 = [self.ws2[i] * x * np.sin(self.ws1[i] * x + self.bs1[i]) for i in range(self.size)]
-                d_bs1 = [self.ws2[i] * np.sin(self.ws1[i] * x + self.bs1[i]) for i in range(self.size)]
-                d_wc1 = [np.cos(self.wc1[i] * x + self.bc1[i]) for i in range(self.size)]
-                d_wc2 = [self.wc2[i] * x * np.sin(self.wc1[i] * x + self.bc1[i]) for i in range(self.size)]
-                d_bc1 = [self.wc2[i] * np.sin(self.wc1[i] * x + self.bc1[i]) for i in range(self.size)]
+                loss += (p - y) ** 2
+                d += 2 * (p - y)
+                d_ws1 = [np.cos(self.ws1[i] * x + self.bs1[i]) * self.ws2[i] * x for i in range(self.size)]
+                d_ws2 = [np.sin(self.ws1[i] * x + self.bs1[i]) for i in range(self.size)]
+                d_bs1 = [np.cos(self.ws1[i] * x + self.bs1[i]) * self.ws2[i] for i in range(self.size)]
+                d_wc1 = [np.sin(self.wc1[i] * x + self.bc1[i]) * self.wc2[i] * x * -1 for i in range(self.size)]
+                d_wc2 = [np.cos(self.wc1[i] * x + self.bc1[i]) for i in range(self.size)]
+                d_bc1 = [np.sin(self.wc1[i] * x + self.bc1[i]) * self.ws2[i] * -1 for i in range(self.size)]
                 self.ws1 = [self.ws1[i] - lr * d * d_ws1[i] for i in range(self.size)]
                 self.ws2 = [self.ws2[i] - lr * d * d_ws2[i] for i in range(self.size)]
                 self.bs1 = [self.bs1[i] - lr * d * d_bs1[i] for i in range(self.size)]
                 self.wc1 = [self.wc1[i] - lr * d * d_wc1[i] for i in range(self.size)]
                 self.wc2 = [self.wc2[i] - lr * d * d_wc2[i] for i in range(self.size)]
                 self.bc1 = [self.bc1[i] - lr * d * d_bc1[i] for i in range(self.size)]
+            print('current loss: %s' % (loss / self.size))
+
+    def inspect(self):
+        print('---------- params ----------')
+        for i in range(self.size):
+            print('%s·Sin(%s·x + %s) + %s·Cos(%s·x + %s)' % (round(self.ws2[i], 4), round(self.ws1[i], 4), round(self.bs1[i], 4), round(self.wc2[i], 4), round(self.wc1[i], 4), round(self.bc1[i], 4)))
 
     def _tri(self, x):
-        y = sum([self.ws2[i] * np.sin(self.ws1[i] * x + self.bs1[i]) + self.wc2[i] * np.cos(self.wc1[i] * x + self.bc1[i]) for i in range(self.size)])
+        y = sum(
+            [self.ws2[i] * np.sin(self.ws1[i] * x + self.bs1[i]) + self.wc2[i] * np.cos(self.wc1[i] * x + self.bc1[i])
+             for i in range(self.size)])
         return y
 
 
 if __name__ == '__main__':
     tri = TrigonometricCurveFit()
-    par = [1./i for i in range(1, 10)]
-    x = [0, 1, 2, 3, 4, 5]
-    y = [0, 2, 1, -2, 0, 1.5]
-    s = [i/100. for i in range(500)]
+    par = [1. / i for i in range(1, 10)]
+    y = [0.5825, 0.5571, 0.5186, 0.5395, 0.5206, 0.5287, 0.5033, 0.5193, 0.5176]
+    # y = [y[0]] * 10 + y + [y[-1]] * 10
+    x = [i for i in range(len(y))]
+    r = [i / 100. for i in range(len(x) * 100)]
+    # s = [e - len(x) for e in r] + r + [e + len(x) for e in r]
+    s = r
     tri.build(par)
     tri.optimize(x, y, iter_nums=1000, lr=0.001)
+    tri.inspect()
     t = tri.fit(s)
     plt.plot(x, y)
     plt.plot(s, t)
     plt.show()
-
-
-
