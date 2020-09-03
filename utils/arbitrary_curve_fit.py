043d62a4c72a6295b741e9d58bdbecf5549a80b1
hallazie <hallazie@outlook.com>
2020-09-03 15:59:25 +0800

    --amend

--- utils/arbitrary_curve_fit.py
+++ utils/arbitrary_curve_fit.py
@@ -84,7 +84,7 @@ class AlbitraryCurveFit:
             logger.info(x)
 
 
-def test_trigono():
+def build_trigono():
     fitter = AlbitraryCurveFit()
     f = lambda p, x: p[0] * np.sin(p[1] * x + p[2]) + p[3] * np.cos(p[4] * x + p[5])
     d_list = [
@@ -96,46 +96,58 @@ def test_trigono():
         lambda p, x: p[3] * np.sin(p[4] * x + p[5]) * -1
     ]
     fitter.build(f, d_list, 6, 20)
-    y_list = [0.5825, 0.5571, 0.5186, 0.5395, 0.5206, 0.5287, 0.5033, 0.5193, 0.5176]
-    x_list = [i for i in range(len(y_list))]
-    z_list = [i / 100. for i in range(len(x_list) * 100)]
-    fitter.optimize(x_list, y_list, lr=0.001)
-    fitter.inspect()
-    p_list = fitter.fit(z_list)
-    plt.plot(x_list, y_list)
-    plt.plot(z_list, p_list)
-    plt.show()
+    return fitter
 
 
-def test_sigmoid():
+def build_sigmoid():
     fitter = AlbitraryCurveFit()
     f = lambda p, x: ((1 + np.e ** (p[0] * x + p[1])) ** -1) * p[2]
     d_list = [
-        lambda p, x: ((1 + np.e ** (p[0] * x)) ** -2) * x * p[2] * -1,
-        lambda p, x: ((1 + np.e ** (p[0] * x)) ** -2) * p[2] * -1,
-        lambda p, x: (1 + np.e ** (p[0] * x)) ** -1
+        lambda p, x: ((1 + np.e ** (p[0] * x + p[1])) ** -2) * np.e ** (p[0] * x + p[1]) * x * p[2] * -1,
+        lambda p, x: ((1 + np.e ** (p[0] * x + p[1])) ** -2) * np.e ** (p[0] * x + p[1]) * p[2] * -1,
+        lambda p, x: (1 + np.e ** (p[0] * x + p[1])) ** -1
     ]
-    fitter.build(f, d_list, 3, 256)
-    # y_list = [0.5825, 0.5571, 0.5186, 0.5395, 0.5206, 0.5287, 0.5033, 0.5193, 0.5176]
-    # x_list = [i for i in range(len(y_list))]
-    # z_list = [i / 100. for i in range(len(x_list) * 100)]
+    fitter.build(f, d_list, 3, 32)
+    return fitter
+
+
+def build_tanh():
+    fitter = AlbitraryCurveFit()
+    f = lambda p, x: p[0] * np.tanh(p[1] * x + p[2])
+    d_list = [
+        lambda p, x: np.tanh(p[1] * x + p[2]),
+        lambda p, x: p[0] * (1 - np.tanh(p[1] * x + p[2]) ** 2) * x,
+        lambda p, x: p[0] * (1 - np.tanh(p[1] * x + p[2]) ** 2)
+    ]
+    fitter.build(f, d_list, 3, 128)
+    return fitter
+
+
+def test():
+    fitter = build_sigmoid()
+
+    # y_list = [65.3061224489796, 58.1632653061225, 57.8656462585034, 62.2874149659864, 71.4710884353742, 62.1173469387755, 56.8452380952381, 55.4421768707483, 59.7789115646259]
+    # x_list = [-3, 3, 5, 7, 9, 15, 27, 39, 55]
+    # y_list = [e/100. for e in y_list]
+    # x_list = [e-min(x_list) for e in x_list]
+
     y_list = [0.859875904860393, 0.8839193381592549, 0.915460186142709, 0.905377456049638, 0.8815925542916241, 0.8989141675284391, 0.8451396070320579, 0.8650465356773529, 0.825491209927611]
     x_list = [0, 1, 3, 5, 7, 13, 26, 37, 52]
-    z_list = [i / 100. for i in range(min(x_list) * 100, (max(x_list) + 1) * 100)]
-    fitter.optimize(x_list, y_list, iter_num=1000, lr=0.01)
-    fitter.inspect()
-    p_list = fitter.fit(z_list)
-    plt.plot(x_list, y_list)
-    plt.plot(z_list, p_list)
-    plt.show()
 
-
-def test_tanh():
-    pass
+    z_list = [i / 100. for i in range(min(x_list) * 100, (max(x_list) + 1) * 200)]
+    for i in range(10):
+        fitter.optimize(x_list, y_list, iter_num=1000, lr=0.005)
+        fitter.inspect()
+        p_list = fitter.fit(z_list)
+        plt.plot(x_list, y_list)
+        plt.plot(z_list, p_list)
+        # plt.show()
+        plt.savefig('../output/%s.jpg' % i)
+        plt.clf()
 
 
 if __name__ == '__main__':
-    test_sigmoid()
+    test()
 
 
 
