8bcc28a854ac71b7fc19276259c9851e08205626
hallazie <hallazie@outlook.com>
2020-09-04 16:54:24 +0800

    --amend

--- utils/arbitrary_curve_fit.py
+++ utils/arbitrary_curve_fit.py
@@ -2,9 +2,9 @@
 # @author: xiao shanghua
 
 from config import logger
+from utils.arbitrary_curve_fit_builder import *
 
 import matplotlib.pyplot as plt
-import pickle as pkl
 import dill
 import numpy as np
 
@@ -102,45 +102,6 @@ class AlbitraryCurveFit:
             self.coeff = model.get('coeff', None)
             if not callable(self._base):
                 raise TypeError('init fitter failed, type confirmation failed')
-
-
-def build_trigono():
-    fitter = AlbitraryCurveFit()
-    f = lambda p, x: p[0] * np.sin(p[1] * x + p[2]) + p[3] * np.cos(p[4] * x + p[5])
-    d_list = [
-        lambda p, x: np.sin(p[1] * x + p[2]),
-        lambda p, x: p[0] * np.cos(p[1] * x + p[2]) * x,
-        lambda p, x: p[0] * np.cos(p[1] * x + p[2]),
-        lambda p, x: np.cos(p[4] * x + p[5]),
-        lambda p, x: p[3] * np.sin(p[4] * x + p[5]) * x * -1,
-        lambda p, x: p[3] * np.sin(p[4] * x + p[5]) * -1
-    ]
-    fitter.build(f, d_list, 6, 20)
-    return fitter
-
-
-def build_sigmoid():
-    fitter = AlbitraryCurveFit()
-    f = lambda p, x: ((1 + np.e ** (p[0] * x + p[1])) ** -1) * p[2]
-    d_list = [
-        lambda p, x: ((1 + np.e ** (p[0] * x + p[1])) ** -2) * np.e ** (p[0] * x + p[1]) * x * p[2] * -1,
-        lambda p, x: ((1 + np.e ** (p[0] * x + p[1])) ** -2) * np.e ** (p[0] * x + p[1]) * p[2] * -1,
-        lambda p, x: (1 + np.e ** (p[0] * x + p[1])) ** -1
-    ]
-    fitter.build(f, d_list, 3, 32)
-    return fitter
-
-
-def build_tanh():
-    fitter = AlbitraryCurveFit()
-    f = lambda p, x: p[0] * np.tanh(p[1] * x + p[2])
-    d_list = [
-        lambda p, x: np.tanh(p[1] * x + p[2]),
-        lambda p, x: p[0] * (1 - np.tanh(p[1] * x + p[2]) ** 2) * x,
-        lambda p, x: p[0] * (1 - np.tanh(p[1] * x + p[2]) ** 2)
-    ]
-    fitter.build(f, d_list, 3, 128)
-    return fitter
 
 
 def test():
