c901d1dee8091074bcb0407be4dd49d2265d52f2
hallazie <hallazie@outlook.com>
2021-01-25 23:57:57 +0800

    --amemd

--- auto_encoder_approach.py
+++ auto_encoder_approach.py
@@ -5,42 +5,52 @@
 # @time: 2020/12/21 21:22
 # @desc:
 
-from sklearn.model_selection import train_test_split
 from config import *
 from score import scoring
-from blocks import *
+from dataloader import DataLoader
+from blocks import AutoEncoder
 
 import torch
 import torch.nn as nn
 import numpy as np
-import datatable as dt
 import os
-
-train = dt.fread(TRAIN_PATH)
-train = train.to_pandas()
-train = train[train['weight'] != 0]
-train['action'] = ((train['weight'].values * train['resp'].values) > 0).astype('int')
-
-X_train = train.loc[:, train.columns.str.contains('feature')]
-y_train = train.loc[:, 'action']
-w_train = train.loc[:, 'weight']
-r_train = train.loc[:, 'resp']
-d_train = train.loc[:, 'date']
-
-X_train, X_test, y_train, y_test, w_train, w_test, r_train, r_test, d_train, d_test = train_test_split(X_train, y_train, w_train, r_train, d_train, random_state=666, test_size=0.08)
-X_train = X_train.fillna(0)
-X_test = X_test.fillna(0)
-
-print(f'train and test set init finished with size: {X_train.shape} {y_train.shape}, {X_test.shape} {y_test.shape}')
-print(f'train date: {len(set(d_train))}, test date: {len(set(d_test))}, datatype={type(X_train)}')
+import warnings
 
 
-auto_encoder = AutoEncoder(inp_size=130, out_size=1)
-
-optimizer = torch.optim.Adam(params=auto_encoder.parameters())
-criterion = torch.nn.MSELoss()
+warnings.filterwarnings("ignore")
 
 
+loader = DataLoader()
+
+auto_encoder = AutoEncoder(inp_size=130, out_size=5).cuda()
+# auto_encoder = torch.load('checkpoints/autoencoder-base.pkl').cuda()
+auto_encoder.train()
+
+optimizer = torch.optim.Adam(params=auto_encoder.parameters(), lr=1e-3)
+criterion1 = torch.nn.MSELoss()
+criterion2 = torch.nn.BCELoss()
+
+epoches = 1000
+batchsize = 4096
+datasize = loader.X_train.shape[0]
+
+for e in range(epoches):
+    for b in range(datasize//batchsize):
+        auto_encoder.zero_grad()
+        x, y = loader.X_train[b*batchsize:(b+1)*batchsize], loader.y_train[b*batchsize:(b+1)*batchsize]
+        x = torch.tensor(x.values).float().cuda()
+        # y = torch.unsqueeze(torch.tensor(y.values), 1).float().cuda()
+        y = torch.tensor(y).float().cuda()
+        p, d = auto_encoder(x)
+        loss1 = criterion1(d, x)
+        loss2 = criterion2(p, y)
+        loss = loss1 + loss2
+        loss.backward()
+        optimizer.step()
+        if b % 100 == 0:
+            print(f'epoch={e}, batch={b}, mse={loss1.data}, bce={loss2.data} loss={loss.data}')
+    if (e+1) % 20 == 0 and e > 0:
+        torch.save(auto_encoder, f'checkpoints/autoencoder-{e+1}.pkl')
 
 
 if __name__ == '__main__':
